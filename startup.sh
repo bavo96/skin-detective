4597facbe3dcb313b77e89ecf3efa6bddd0d0e1c
elonmusk9966 <122256796+elonmusk9966@users.noreply.github.com>
2023-01-10 02:42:49 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install ffmpeg libsm6 libxext6  -y
+pip install grad-cam
 GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' -c /opt/startup/gunicorn.conf.py --chdir=. -k uvicorn.workers.UvicornWorker" gunicorn app:app
