bda7fa2b13fc866c4c5d8c699f28512717ff209c
elonmusk9966 <122256796+elonmusk9966@users.noreply.github.com>
2023-01-09 15:40:46 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
+sudo apt-get update
+sudo apt-get install ffmpeg libsm6 libxext6  -y
 GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' -c /opt/startup/gunicorn.conf.py --chdir=. -k uvicorn.workers.UvicornWorker" gunicorn app:app
